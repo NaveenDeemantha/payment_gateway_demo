c5d74e977fd58e0e1eb8056cf0a4d2de25da2abf
Naveen <naveenpunchihewa9@gmail.com>
2025-04-22 16:23:17 +0530

    Integrated Payment gateway

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/checkout', 'App\Http\Controllers\StripeController@checkout')->name('checkout');
+Route::post('/session', 'App\Http\Controllers\StripeController@session')->name('session');
+Route::get('/success', 'App\Http\Controllers\StripeController@success')->name('success');
